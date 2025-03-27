65a5eff6cd3e768357978b4c4634dc3dd8780dcc
LiixAX23 <schurtzjoao@gmail.com>
2025-03-27 09:37:17 -0300

    Update db.php
    Atualização do db.php com dados do novo banco freeSQL

--- db.php
+++ db.php
@@ -1,15 +1,16 @@
 <?php
 // Informações do banco de dados
-$host = "localhost";           // No seu caso, como está usando local, é localhost
-$dbname = "inztagro_com";      // Nome do seu banco de dados
-$user = "root";                // Usuário padrão do MariaDB no local
-$password = "";                // Senha do banco (em branco se não definiu)
+$host = 'sql10.freesqldatabase.com';           // No seu caso, como está usando local, é localhost
+$dbname = 'sql10769891';       // Nome do seu banco de dados
+$user = 'sql10769891';                // Usuário padrão do MariaDB no local
+$password = '14K!SeAg8i';                // Senha do banco (em branco se não definiu)
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Conexão bem-sucedida!"; // Pode comentar ou remover
 } catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
 }
 ?>
+
