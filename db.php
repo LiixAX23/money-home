3523e5680289359bb7a16ad41e0f43996cd53893
LiixAX23 <schurtzjoao@gmail.com>
2025-03-27 09:44:43 -0300

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $host = 'sql10.freesqldatabase.com';           // No seu caso, como está usando local, é localhost
 $dbname = 'sql10769891';       // Nome do seu banco de dados
 $user = 'sql10769891';                // Usuário padrão do MariaDB no local
-$password = '14K!SeAg8i';                // Senha do banco (em branco se não definiu)
+$password = 'l4KISeAg8i';                // Senha do banco (em branco se não definiu)
 
 try {
     $pdo = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8", $user, $password);
